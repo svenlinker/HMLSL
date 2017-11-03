324b4a29bc3ef96cec8e11d6075587f9e74526c0
Sven Linker <s.linker@liverpool.ac.uk>
2017-11-03 16:49:52 +0000

    fixed layout for Move.thy

--- Move.thy
+++ Move.thy
@@ -26,7 +26,10 @@ begin
 lemmas[simp] = shift_dict length_dict
 
 definition move::"traffic \<Rightarrow> traffic \<Rightarrow> view \<Rightarrow> view"
-  where "move ts ts' v = \<lparr> ext = shift (ext v) ((pos ts' (own v)) - pos ts (own v)), lan = lan v, own =own v \<rparr>"
+  where 
+    "move ts ts' v = \<lparr> ext = shift (ext v) ((pos ts' (own v)) - pos ts (own v)), 
+                       lan = lan v, 
+                       own =own v \<rparr>"
 
 lemma move_keeps_length:"\<parallel>ext v\<parallel> = \<parallel>ext (move ts ts' v)\<parallel>"
   using real_int.shift_keeps_length by (simp add: move_def)
@@ -37,25 +40,40 @@ lemma move_keeps_owner:"own v = own (move ts ts' v)" using move_def by simp
 
 lemma move_nothing :"move ts ts v = v" using real_int.shift_zero move_def by simp
 
-lemma move_trans:"(ts \<^bold>\<Rightarrow> ts') \<and> (ts' \<^bold>\<Rightarrow>ts'') \<longrightarrow> move ts' ts'' (move ts ts' v) = move ts ts'' v"
+lemma move_trans:
+  "(ts \<^bold>\<Rightarrow> ts') \<and> (ts' \<^bold>\<Rightarrow>ts'') \<longrightarrow> move ts' ts'' (move ts ts' v) = move ts ts'' v"
 proof
   assume assm:"(ts \<^bold>\<Rightarrow> ts') \<and> (ts' \<^bold>\<Rightarrow>ts'')"
-  have "(pos ts'' (own v)) - pos ts (own v) = (pos ts'' (own v) + pos ts' (own v)) - ( pos ts (own v) + pos ts' (own v))"
+  have 
+    "(pos ts'' (own v)) - pos ts (own v) 
+      = (pos ts'' (own v) + pos ts' (own v)) - ( pos ts (own v) + pos ts' (own v))"
     by simp
-  have "move ts' ts'' (move ts ts' v) =
- \<lparr> ext = shift (ext (move ts ts' v)) (pos ts'' (own (move ts ts' v)) - pos ts' (own (move ts ts' v))),
-  lan = lan (move ts ts' v), own = own (move ts ts' v) \<rparr> " using move_def by blast
+  have 
+    "move ts' ts'' (move ts ts' v) =
+    \<lparr> ext = 
+    shift (ext (move ts ts' v)) 
+      (pos ts'' (own (move ts ts' v)) - pos ts' (own (move ts ts' v))),
+     lan = lan (move ts ts' v), 
+     own = own (move ts ts' v) \<rparr> " 
+    using move_def by blast
   hence "move ts' ts'' (move ts ts' v) = 
  \<lparr> ext = shift (ext (move ts ts' v)) (pos ts'' (own v) - pos ts' (own  v)),
-  lan = lan  v, own = own  v \<rparr> " using move_def by simp
+  lan = lan  v, own = own  v \<rparr> " 
+    using move_def by simp
   then show "move ts' ts'' (move ts ts' v) = move ts ts'' v"  
   proof -
     have f2: "\<forall>x0 x1. (x1::real) + x0 = x0 + x1"
       by auto
-    have "pos ts'' (own v) + - 1 * pos ts' (own v) + (pos ts' (own v) + - 1 * pos ts (own v)) = pos ts'' (own v) + - 1 * pos ts (own v)"
+    have 
+      "pos ts'' (own v)
+          + -1*pos ts' (own v)+(pos ts' (own v) + -1*pos ts (own v)) 
+        = pos ts'' (own v) + - 1 * pos ts (own v)"
       by auto
-    then have "real_int.shift (ext v) (pos ts'' (own v) + - 1 * pos ts (own v)) = real_int.shift (real_int.shift (ext v) (pos ts' (own v) + - 1 * pos ts (own v))) (pos ts'' (own v) + - 1 * pos ts' (own v))"
-      using f2 real_int.shift_additivity  by (metis (no_types) \<open>\<And>y x r. real_int.shift r (x + y) = real_int.shift (real_int.shift r x) y\<close>)
+    then have 
+      "(shift (ext v) ((pos ts'' (own v)) + ( -1 *  pos ts (own v)))) = 
+       shift (shift  (ext v) (pos ts' (own v) + - 1 * pos ts (own v))) 
+          (pos ts'' (own v) + - 1 * pos ts' (own v))"  
+      by (metis f2 real_int.shift_additivity shift_dict)
     then show ?thesis
       using move_def f2 by simp
   qed
